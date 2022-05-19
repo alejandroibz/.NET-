8512c2e80c8166ae049882062a6661a27a1b5f70
Alejandro Ibañez <coder.alejandro@gmail.com>
2022-05-18 22:39:19 -0300

    ejercicio clase 5

---
+++ clase5-ejercicio/clase5-ejercicio/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+string rta = "S";
+while (rta.ToUpper() == "S")
+{
+    Console.Clear();
+
+    Console.WriteLine("Ingrese su nombre por favor. :)");
+    var nombre = Console.ReadLine();
+
+    Console.WriteLine("Hola " + nombre + ", mucho gusto! Un placer conocerte");
+    Console.WriteLine("Desea usted " + nombre + " continuar? Si la respuesta es SI por favor ingrese 'S'. Si la respuesta es NO por favor ingrese 'N' ");
+    rta = Console.ReadLine();
+}
+if (rta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+    Console.WriteLine("Presione una tecla para finalizar la ejecucion");
+    Console.ReadKey();
+    Console.Clear();
+}
+    
+else
+{
+    Console.WriteLine("Opcion no valida");
+}   
+
