1217441076d16f372c962939b0d4244ad2961260
Alejandro Ibañez <coder.alejandro@gmail.com>
2022-05-20 20:31:08 -0300

    clase 6

---
+++ clase6/clase6/Program.cs
@@ -0,0 +1,23 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+int contador = 0;
+int numero;
+do
+{
+    Console.WriteLine("Intenta adivinar el numero (1 al 20)");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero < numeroSecreto)
+    {
+        Console.WriteLine("Te quedaste corto. Proba con un numero más grande");
+    }
+
+    else if (numero > numeroSecreto)
+    {
+        Console.WriteLine("Te pasaste. Proba con un numero más chico");
+    }
+
+    contador++;
+}
+while (numeroSecreto != numero);
+
+Console.WriteLine($"Felicidades has adviniado el numero es {numeroSecreto}. Tan solo te tomo {contador} intento(s)");
